3b64d5ced85edc743b2132bd1dbc4eb05ddc1b8c
Sommerregen <sommerregen@users.noreply.github.com>
2015-10-02 13:38:59 +0200

    Updated Composer

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit6700e2db8f9854631814b0634f09ec0b::getLoader();
+return ComposerAutoloaderInit0515c889ea36eb42c6a70ab2f0df901d::getLoader();
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Grav\\Plugin\\Login\\' => array($baseDir . '/classes'),
 );
