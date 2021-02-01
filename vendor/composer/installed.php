9b1c72847837109a667c37b8b985869e6b2b807a
Andy Miller <rhuk@mac.com>
2021-01-31 18:02:53 -0700

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5670bd44d33b738ccf1baaf1615b42f7dbab78cb',
+    'name' => 'getgrav/grav-plugin-login',
+  ),
+  'versions' => 
+  array (
+    'bacon/bacon-qr-code' => 
+    array (
+      'pretty_version' => '1.0.3',
+      'version' => '1.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5a91b62b9d37cee635bbf8d553f4546057250bee',
+    ),
+    'birke/rememberme' => 
+    array (
+      'pretty_version' => '1.0.5',
+      'version' => '1.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '810473852eb4823098e47e23376a19b77ba0c165',
+    ),
+    'getgrav/grav-plugin-login' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5670bd44d33b738ccf1baaf1615b42f7dbab78cb',
+    ),
+    'paragonie/random_compat' => 
+    array (
+      'pretty_version' => 'v1.4.3',
+      'version' => '1.4.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9b3899e3c3ddde89016f576edb8c489708ad64cd',
+    ),
+    'robthree/twofactorauth' => 
+    array (
+      'pretty_version' => '1.7.0',
+      'version' => '1.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '37983bf675c5baca09d19d6705170489d0df0002',
+    ),
+  ),
+);
