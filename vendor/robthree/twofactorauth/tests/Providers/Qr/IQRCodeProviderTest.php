4b08bd1fab01ed825de3bd9379593b70f3c2d128
Matias Griese <matias@trilbymedia.com>
2022-06-14 10:04:39 +0300

    Composer update

--- vendor/robthree/twofactorauth/tests/Providers/Qr/IQRCodeProviderTest.php
+++ vendor/robthree/twofactorauth/tests/Providers/Qr/IQRCodeProviderTest.php
@@ -5,26 +5,11 @@ namespace Tests\Providers\Qr;
 use PHPUnit\Framework\TestCase;
 use RobThree\Auth\TwoFactorAuth;
 use RobThree\Auth\TwoFactorAuthException;
+use RobThree\Auth\Providers\Qr\HandlesDataUri;
 
 class IQRCodeProviderTest extends TestCase
 {
-    /**
-     * @param string $datauri
-     *
-     * @return null|array
-     */
-    private function DecodeDataUri($datauri)
-    {
-        if (preg_match('/data:(?P<mimetype>[\w\.\-\/]+);(?P<encoding>\w+),(?P<data>.*)/', $datauri, $m) === 1) {
-            return array(
-                'mimetype' => $m['mimetype'],
-                'encoding' => $m['encoding'],
-                'data' => base64_decode($m['data'])
-            );
-        }
-
-        return null;
-    }
+    use HandlesDataUri;
 
     /**
      * @return void
@@ -38,6 +23,25 @@ class IQRCodeProviderTest extends TestCase
         $this->assertEquals('test/test', $data['mimetype']);
         $this->assertEquals('base64', $data['encoding']);
         $this->assertEquals('otpauth://totp/Test%26Label?secret=VMR466AB62ZBOKHE&issuer=Test%26Issuer&period=30&algorithm=SHA1&digits=6@200', $data['data']);
+    }
+
+    /**
+     * @return void
+     */
+    public function testTotpUriIsCorrectNoIssuer()
+    {
+        $qr = new TestQrProvider();
+
+        /**
+         * The library specifies the issuer is null by default however in PHP 8.1
+         * there is a deprecation warning for passing null as a string argument to rawurlencode
+         */
+
+        $tfa = new TwoFactorAuth(null, 6, 30, 'sha1', $qr);
+        $data = $this->DecodeDataUri($tfa->getQRCodeImageAsDataUri('Test&Label', 'VMR466AB62ZBOKHE'));
+        $this->assertEquals('test/test', $data['mimetype']);
+        $this->assertEquals('base64', $data['encoding']);
+        $this->assertEquals('otpauth://totp/Test%26Label?secret=VMR466AB62ZBOKHE&issuer=&period=30&algorithm=SHA1&digits=6@200', $data['data']);
     }
 
     /**
